198662f5e8b50609bf6b44b9242fa3966ef042de
Nitesh Parmar <ntsparmar@gmail.com>
2023-08-30 20:08:51 +0530

    portfolio update

---
+++ data.js
@@ -0,0 +1,284 @@
+
+        var videoData =
+[
+ {
+  category: "Multiplayer",
+  name: "Fans of Soccer: Online Football Disc Challenge",
+  url: "https://youtu.be/8gLnGOXmBHA",
+  description: "null",
+  androidLink: "https://play.google.com/store/apps/details?id=com.omentertainment.soccerfans",
+  iosLink: "null"
+ },
+ {
+  category: "Multiplayer",
+  name: "Qikplay - Win Real Gift Cards",
+  url: "https://drive.google.com/file/d/1opdPc55r1kkWrx0zmgRypzEHM0cL2FDP/view?usp=sharing",
+  description: "null",
+  androidLink: "https://play.google.com/store/apps/details?id=com.Qikplay.LudoandUNO",
+  iosLink: "null"
+ },
+ {
+  category: "Multiplayer",
+  name: "ISTO3D-WiFi Multiplayer Game",
+  url: "https://drive.google.com/file/d/1W95XHjGwVNYnLGOan3JVsy74WUaMpxxC/view?usp=sharing",
+  description: "null",
+  androidLink: "https://play.google.com/store/apps/details?id=com.moxmind.isto&authuser=0",
+  iosLink: "null"
+ },
+ {
+  category: "AR",
+  name: "Modi ki Note - Modi Rocks",
+  url: "https://drive.google.com/file/d/1pB2NKMAmPKe1j_jINNhTGM2nEy9IPbwI/view?usp=sharing",
+  description: "null",
+  androidLink: "https://play.google.com/store/apps/details?id=com.MoxMind.ModiRocksRocks&authuser=0",
+  iosLink: "null"
+ },
+ {
+  category: "AR",
+  name: "AR Furniture Placing",
+  url: "https://drive.google.com/file/d/14U_MxCLAjtQZipEAK8PzRHxWL448UiDz/view?usp=sharing",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "AR",
+  name: "Cute Throw Game",
+  url: "https://drive.google.com/file/d/1y8bn_Ja4fR3oYtQtrQnzpjgF7pMTzB1n/view?usp=sharing",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "AR",
+  name: "AR Vehicle Classification",
+  url: "https://drive.google.com/file/d/1RTHz1KSkvHv-o7oACE0hw69sGH1v0LN3/view?usp=sharing",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "VR",
+  name: "VR - Home Interior",
+  url: "https://drive.google.com/file/d/1EU0lvxYPJO790nI_AfqCet6Zq0QYbPVZ/view?usp=sharing",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "VR",
+  name: "The Bug Butcher",
+  url: "https://drive.google.com/file/d/1SBmejxeJyrP4UlQTolKfvKq_1PfQ39aP/view?usp=sharing",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Maggots Fight",
+  url: "https://youtu.be/Lvf_95wUm8o",
+  description: "null",
+  androidLink: "https://play.google.com/store/apps/details?id=com.redlinegames.bugsattack3d",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Oil Searcher",
+  url: "https://youtu.be/lp00xddJ2as",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Pandora Box",
+  url: "https://youtu.be/lNOO9BWERLI",
+  description: "null",
+  androidLink: "https://play.google.com/store/apps/details?id=com.redlinegames.pandorarun",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Monster Land",
+  url: "https://youtu.be/x7V9x7FC2J8",
+  description: "null",
+  androidLink: "https://play.google.com/store/apps/details?id=com.redlinegames.fightevolution",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Kaiju Fight",
+  url: "https://youtu.be/6PblH2-3zb0",
+  description: "null",
+  androidLink: "https://play.google.com/store/apps/details?id=com.redlinegames.kaijufight",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "KickEm",
+  url: "https://youtu.be/LGgkUQCjKAc",
+  description: "null",
+  androidLink: "https://play.google.com/store/apps/details?id=com.redlinegames.kickem3d",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Mission Possible",
+  url: "https://youtu.be/ESpBEwi_3eQ",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Crowd Painter",
+  url: "https://youtu.be/LhSM2yHm-EM",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Hyper Tailor",
+  url: "https://youtu.be/-Z60EjsRA20",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Reverse Painting",
+  url: "https://youtu.be/bQQGwju3-nQ",
+  description: "null",
+  androidLink: "https://play.google.com/store/apps/details?id=com.redlinegames.reversepainting",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Farmio",
+  url: "https://youtu.be/w1JXdB9eGMg",
+  description: "null",
+  androidLink: "https://play.google.com/store/apps/details?id=farm.io.battle",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Stop the train",
+  url: "https://youtu.be/gXVshQhQieU",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Cheerleader tower",
+  url: "https://youtu.be/OOgwpp-YF7c",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Erase story",
+  url: "https://youtu.be/CaAbiW_CA1E",
+  description: "null",
+  androidLink: "https://play.google.com/store/apps/details?id=com.redlinegames.erasestory",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Shortcut Relay",
+  url: "https://youtu.be/BggkkSVJPyA",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Quicktestfingure",
+  url: "https://youtu.be/V85j3dMAl0M",
+  description: "null",
+  androidLink: "https://apps.apple.com/us/app/quickest-fingers/id1545351768",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Epic Fall-",
+  url: "https://youtu.be/fQRElQohpt4",
+  description: "null",
+  androidLink: "https://apps.apple.com/us/app/epic-fall/id1545332345",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Turbo man",
+  url: "https://youtu.be/st1mfPCNqrg",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Walk Master3d",
+  url: "https://youtu.be/u_K_w5P-4E0",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Drawmbush",
+  url: "https://youtu.be/lKEhyPsljiY",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "Hypercasual",
+  name: "Magic Monsters 3D",
+  url: "https://youtu.be/x7V9x7FC2J8",
+  description: "null",
+  androidLink: "https://play.google.com/store/apps/details?id=com.RedLineGames.MagicMonsters3D",
+  iosLink: "null"
+ },
+ {
+  category: "Casual",
+  name: "MOWGLI JUMP - JUNGLE BOOK GAME",
+  url: "http://igeniusdev.com/portfolio/mowgli-jump-jungle-book-game",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "Casual",
+  name: "Super hexa fit",
+  url: "https://itunes.apple.com/in/app/super-hexa-fit/id1111116865?mt=8",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "Casual",
+  name: "Dont vapid up",
+  url: "https://drive.google.com/file/d/1TzF7Ff_JgqIKQIn3y_fe9MdJdAowWpJP/view?usp=sharing",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ },
+ {
+  category: "Casual",
+  name: "Flappy magic bird",
+  url: "null",
+  description: "null",
+  androidLink: "https://itunes.apple.com/in/app/flappy-magic-bird/id1004543881?mt=8",
+  iosLink: "https://itunes.apple.com/in/app/flappy-magic-bird/id1004543881?mt=8"
+ },
+ {
+  category: "Casual",
+  name: "SANTA'S SLAY™ - CHRISTMAS GAME",
+  url: "http://igeniusdev.com/portfolio/santas-slay-christmas-game",
+  description: "null",
+  androidLink: "null",
+  iosLink: "null"
+ }
+];
