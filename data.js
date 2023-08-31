25c5ab9f8d0a87f3b2095c4afd60047a0f1dccb4
Nitesh Parmar <ntsparmar@gmail.com>
2023-09-01 01:21:40 +0530

    website updated

--- data.js
+++ data.js
@@ -1,304 +1,483 @@
 
         var videoData =[
  {
-  category: "Multiplayer",
+  category: "Multiplayer,3D",
   name: "Fans of Soccer: Online Football Disc Challenge",
   url: "https://www.youtube.com/embed/hl0tz3163vQ",
-  description: "We can add image as a link and other HTML elements as a link. A link is a connection from one Web page to another web page.We can add image as a link and other HTML elements as a link. A link is a connection from one Web page to another web page.",
+  description: "niteshparmar",
   androidLink: "https://play.google.com/store/apps/details?id=com.omentertainment.soccerfans",
   iosLink: "null"
  },
  {
-  "category": "Multiplayer",
+  "category": "Multiplayer,2D",
   "name": "Qikplay - Win Real Gift Cards",
   "url": "https://www.youtube.com/embed/yabdeTjGhek",
   "description": "null",
-  androidLink: "https://play.google.com/store/apps/details?id=com.Qikplay.LudoandUNO",
-  iosLink: "null"
+  "androidLink": "https://play.google.com/store/apps/details?id=com.Qikplay.LudoandUNO",
+  "iosLink": "null"
  },
  {
-  "category": "Multiplayer",
+  "category": "Multiplayer,3D",
   "name": "ISTO3D-WiFi Multiplayer Game",
   "url": "https://www.youtube.com/embed/H7t18n0Rl6U",
   "description": "null",
-  androidLink: "https://play.google.com/store/apps/details?id=com.moxmind.isto&authuser=0",
-  iosLink: "null"
+  "androidLink": "https://play.google.com/store/apps/details?id=com.moxmind.isto&authuser=0",
+  "iosLink": "null"
+ },
+ {
+  "category": "Multiplayer",
+  "name": "Fish Game",
+  "url": "https://www.youtube.com/embed/Oh1gzRQcsuY",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "AR",
   "name": "Modi ki Note - Modi Rocks",
   "url": "https://www.youtube.com/embed/tvcmQ9ttPHc",
   "description": "null",
-  androidLink: "https://play.google.com/store/apps/details?id=com.MoxMind.ModiRocksRocks&authuser=0",
-  iosLink: "null"
+  "androidLink": "https://play.google.com/store/apps/details?id=com.MoxMind.ModiRocksRocks&authuser=0",
+  "iosLink": "null"
  },
  {
   "category": "AR",
   "name": "AR Furniture Placing",
   "url": "https://www.youtube.com/embed/zQyiA3mE6KY",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "AR",
   "name": "Cube Throw Game",
   "url": "https://www.youtube.com/embed/SjSTNkY-P78",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "AR",
   "name": "AR Vehicle Classification",
   "url": "https://www.youtube.com/embed/lN1BTqv5haU",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "VR",
   "name": "VR - Home Interior",
   "url": "https://www.youtube.com/embed/efqDzqq10F4",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "VR",
   "name": "The Bug Butcher",
   "url": "https://www.youtube.com/embed/FVrF3efVAVM",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "casino",
+  "name": "Raptor-Firelink slot",
+  "url": "https://www.youtube.com/embed/G5pBg2vddzo",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "casino",
+  "name": "Cosmoslot-Slot,Keno,Fish",
+  "url": "https://www.youtube.com/embed/8D8YgmX7jjA",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "casino",
+  "name": "BoomWins-Slot,Keno,Fish",
+  "url": "https://www.youtube.com/embed/NeunV8q78Tk",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "casino",
+  "name": "Cascade Slot",
+  "url": "https://www.youtube.com/embed/bcQnWN162F4",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "casino",
+  "name": "Atlantis-Firelink Slot",
+  "url": "https://www.youtube.com/embed/xKIC9XQpIis",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Maggots Fight",
   "url": "https://www.youtube.com/embed/Lvf_95wUm8o",
   "description": "null",
-  androidLink: "https://play.google.com/store/apps/details?id=com.redlinegames.bugsattack3d",
-  iosLink: "null"
+  "androidLink": "https://play.google.com/store/apps/details?id=com.redlinegames.bugsattack3d",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Oil Searcher",
   "url": "https://www.youtube.com/embed/lp00xddJ2as",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Pandora Box",
   "url": "https://www.youtube.com/embed/lNOO9BWERLI",
   "description": "null",
-  androidLink: "https://play.google.com/store/apps/details?id=com.redlinegames.pandorarun",
-  iosLink: "null"
+  "androidLink": "https://play.google.com/store/apps/details?id=com.redlinegames.pandorarun",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Monster Land",
   "url": "https://www.youtube.com/embed/x7V9x7FC2J8",
   "description": "null",
-  androidLink: "https://play.google.com/store/apps/details?id=com.redlinegames.fightevolution",
-  iosLink: "null"
+  "androidLink": "https://play.google.com/store/apps/details?id=com.redlinegames.fightevolution",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Kaiju Fight",
   "url": "https://www.youtube.com/embed/6PblH2-3zb0",
   "description": "null",
-  androidLink: "https://play.google.com/store/apps/details?id=com.redlinegames.kaijufight",
-  iosLink: "null"
+  "androidLink": "https://play.google.com/store/apps/details?id=com.redlinegames.kaijufight",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "KickEm",
   "url": "https://www.youtube.com/embed/LGgkUQCjKAc",
   "description": "null",
-  androidLink: "https://play.google.com/store/apps/details?id=com.redlinegames.kickem3d",
-  iosLink: "null"
+  "androidLink": "https://play.google.com/store/apps/details?id=com.redlinegames.kickem3d",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Mission Possible",
   "url": "https://www.youtube.com/embed/ESpBEwi_3eQ",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Crowd Painter",
   "url": "https://www.youtube.com/embed/LhSM2yHm-EM",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Hyper Tailor",
   "url": "https://www.youtube.com/embed/-Z60EjsRA20",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Reverse Painting",
   "url": "https://www.youtube.com/embed/bQQGwju3-nQ",
   "description": "null",
-  androidLink: "https://play.google.com/store/apps/details?id=com.redlinegames.reversepainting",
-  iosLink: "null"
+  "androidLink": "https://play.google.com/store/apps/details?id=com.redlinegames.reversepainting",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Farmio",
   "url": "https://www.youtube.com/embed/w1JXdB9eGMg",
   "description": "null",
-  androidLink: "https://play.google.com/store/apps/details?id=farm.io.battle",
-  iosLink: "null"
+  "androidLink": "https://play.google.com/store/apps/details?id=farm.io.battle",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Stop the train",
   "url": "https://www.youtube.com/embed/gXVshQhQieU",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Cheerleader tower",
   "url": "https://www.youtube.com/embed/OOgwpp-YF7c",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Erase story",
   "url": "https://www.youtube.com/embed/CaAbiW_CA1E",
   "description": "null",
-  androidLink: "https://play.google.com/store/apps/details?id=com.redlinegames.erasestory",
-  iosLink: "null"
+  "androidLink": "https://play.google.com/store/apps/details?id=com.redlinegames.erasestory",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Shortcut Relay",
   "url": "https://www.youtube.com/embed/BggkkSVJPyA",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Quicktestfingure",
   "url": "https://www.youtube.com/embed/V85j3dMAl0M",
   "description": "null",
-  androidLink: "https://apps.apple.com/us/app/quickest-fingers/id1545351768",
-  iosLink: "null"
+  "androidLink": "https://apps.apple.com/us/app/quickest-fingers/id1545351768",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Epic Fall-",
   "url": "https://www.youtube.com/embed/fQRElQohpt4",
   "description": "null",
-  androidLink: "https://apps.apple.com/us/app/epic-fall/id1545332345",
-  iosLink: "null"
+  "androidLink": "https://apps.apple.com/us/app/epic-fall/id1545332345",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Turbo man",
   "url": "https://www.youtube.com/embed/st1mfPCNqrg",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Walk Master3d",
   "url": "https://www.youtube.com/embed/u_K_w5P-4E0",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Drawmbush",
   "url": "https://www.youtube.com/embed/lKEhyPsljiY",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "Hypercasual",
   "name": "Magic Monsters 3D",
   "url": "https://www.youtube.com/embed/x7V9x7FC2J8",
   "description": "null",
-  androidLink: "https://play.google.com/store/apps/details?id=com.RedLineGames.MagicMonsters3D",
-  iosLink: "null"
+  "androidLink": "https://play.google.com/store/apps/details?id=com.RedLineGames.MagicMonsters3D",
+  "iosLink": "null"
  },
  {
   "category": "Casual",
   "name": "MOWGLI JUMP - JUNGLE BOOK GAME",
   "url": "null",
   "description": "null",
-  androidLink: "http://igeniusdev.com/portfolio/mowgli-jump-jungle-book-game",
-  iosLink: "null"
+  "androidLink": "http://igeniusdev.com/portfolio/mowgli-jump-jungle-book-game",
+  "iosLink": "null"
  },
  {
   "category": "Casual",
   "name": "Super hexa fit",
   "url": "null",
   "description": "null",
-  androidLink: "https://itunes.apple.com/in/app/super-hexa-fit/id1111116865?mt=8",
-  iosLink: "https://itunes.apple.com/in/app/super-hexa-fit/id1111116865?mt=8"
+  "androidLink": "https://itunes.apple.com/in/app/super-hexa-fit/id1111116865?mt=8",
+  "iosLink": "https://itunes.apple.com/in/app/super-hexa-fit/id1111116865?mt=8"
  },
  {
   "category": "Casual",
   "name": "Dont vapid up",
   "url": "https://www.youtube.com/embed/y4h5Njnmz08",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "Casual",
   "name": "Flappy magic bird",
   "url": "null",
   "description": "null",
-  androidLink: "https://itunes.apple.com/in/app/flappy-magic-bird/id1004543881?mt=8",
-  iosLink: "https://itunes.apple.com/in/app/flappy-magic-bird/id1004543881?mt=8"
+  "androidLink": "https://itunes.apple.com/in/app/flappy-magic-bird/id1004543881?mt=8",
+  "iosLink": "https://itunes.apple.com/in/app/flappy-magic-bird/id1004543881?mt=8"
  },
  {
   "category": "Casual",
   "name": "SANTA'S SLAY™ - CHRISTMAS GAME",
   "url": "null",
-  description: "null",
-  androidLink: "http://igeniusdev.com/portfolio/santas-slay-christmas-game",
-  iosLink: "http://igeniusdev.com/portfolio/santas-slay-christmas-game"
+  "description": "null",
+  "androidLink": "http://igeniusdev.com/portfolio/santas-slay-christmas-game",
+  "iosLink": "http://igeniusdev.com/portfolio/santas-slay-christmas-game"
  },
  {
   "category": "Other",
   "name": "Interactive car configuration Product Visualization 360",
   "url": "https://www.youtube.com/embed/3GcaScFV37k",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "Other",
   "name": "Multiplayer City Walk Through",
   "url": "https://www.youtube.com/embed/up-P_01UtCs",
   "description": "null",
-  androidLink: "null",
-  iosLink: "null"
+  "androidLink": "null",
+  "iosLink": "null"
  },
  {
   "category": "Other",
   "name": "Rashi Bazar",
-  "url": "https://www.youtube.com/embed/L4Pou3ALas8"
+  "url": "https://www.youtube.com/embed/L4Pou3ALas8",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "Small Game",
+  "name": "Narcos-Bonus Game",
+  "url": "https://www.youtube.com/embed/L4Pou3ALas8",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "Small Game",
+  "name": "Raptor-Bonus Game",
+  "url": "https://www.youtube.com/embed/2foduOj_Plw",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "Small Game",
+  "name": "Money Train-Bonus Game",
+  "url": "https://www.youtube.com/embed/3NQOc5CO-S8",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "Small Game",
+  "name": "Blood Sucker-Bonus Game",
+  "url": "https://www.youtube.com/embed/n-XI39BK94k",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "Small Game",
+  "name": "Big Bamboo-Bonus Game",
+  "url": "https://www.youtube.com/embed/6-AcuEHlGjs",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "Small Game",
+  "name": "Break Da Bank-Bonus Game",
+  "url": "https://www.youtube.com/embed/Z9v7warrgHY",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "Small Game",
+  "name": "Fortune Wheel-Bonus Game",
+  "url": "https://www.youtube.com/embed/Mux7AHeYsEU",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "Small Game",
+  "name": "Great Circus-Bonus Game",
+  "url": "https://www.youtube.com/embed/PH76ebN9CCw",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "Small Game",
+  "name": "Bahubali-Boar Game",
+  "url": "https://www.youtube.com/embed/2pmZlP_tkMY",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "Small Game",
+  "name": "Jammin Jars-Bonus Game",
+  "url": "https://www.youtube.com/embed/a1eUWyEmUEw",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "Small Game",
+  "name": "Machu Pichu-Bonus Game",
+  "url": "https://www.youtube.com/embed/7CddeintsNA",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "Small Game",
+  "name": "Lucky Gems-Bonus Game",
+  "url": "https://www.youtube.com/embed/DTfO8eUNEDY",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "Small Game",
+  "name": "Party Night-Bonus Game",
+  "url": "https://www.youtube.com/embed/81mC5wFZgkw",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "Small Game",
+  "name": "Spin War-Bonus Game",
+  "url": "https://www.youtube.com/embed/GxDXHQzaBPU",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "Small Game",
+  "name": "Wild Sworm-Bonus Game",
+  "url": "https://www.youtube.com/embed/vcRgyKSSy2I",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
+ },
+ {
+  "category": "Small Game",
+  "name": "XHX-Bonus Game",
+  "url": "https://www.youtube.com/embed/qLvMsihAdeA",
+  "description": "null",
+  "androidLink": "null",
+  "iosLink": "null"
  }
 ];
